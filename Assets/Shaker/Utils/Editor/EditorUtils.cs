567debe4b84a1914efd0edafa2b26bab8a7178cb
Lagache Louis <louislagache@gmail.com>
2020-11-04 16:21:16 +0100

    init

---
+++ Assets/Shaker/Utils/Editor/EditorUtils.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+using UnityEditorInternal;
+using UnityEditor;
+
+namespace Shaker
+{
+    public static class EditorUtils
+    {
+        public static void OpenLockInspector(Object target)
+        {
+            var inspectorType = typeof(Editor).Assembly.GetType("UnityEditor.InspectorWindow");
+            var inspectorInstance = ScriptableObject.CreateInstance(inspectorType) as EditorWindow;
+            inspectorInstance.Show();
+            var prevSelection = Selection.activeGameObject;
+            Selection.activeObject = target;
+            var isLocked = inspectorType.GetProperty("isLocked", BindingFlags.Instance | BindingFlags.Public);
+            isLocked.GetSetMethod().Invoke(inspectorInstance, new object[] { true });
+            Selection.activeGameObject = prevSelection;
+        }
+
+        public static void FocusObject(Object obj)
+        {
+            EditorApplication.ExecuteMenuItem("Window/General/Inspector");
+            AssetDatabase.OpenAsset(obj);
+        }
+
+        [MenuItem("Shaker/Open Terminal")]
+        static void OpenTerminal()
+        {
+
+            if (Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                System.Diagnostics.Process.Start("CMD.exe", "cd " + GetProjectPath());
+            }
+            else if (Application.platform == RuntimePlatform.OSXEditor)
+            {
+                System.Diagnostics.Process.Start(@"/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal", "cd " + GetProjectPath());
+            }
+            else
+            {
+                Debug.LogWarning("Terminal shortcut is not supported on your plateform");
+            }
+        }
+
+        public static string GetProjectPath()
+        {
+            string path = Application.dataPath;
+            path = path.Replace("/Assets", "");
+            return path;
+        }
+
+#if UNITY_EDITOR_OSX
+    [MenuItem("Shaker/Reveal in Finder")]
+#else
+        [MenuItem("Shaker/Show in Explorer")]
+#endif
+        static void OpenInFileExplorer()
+        {
+            EditorUtility.RevealInFinder(GetProjectPath());
+        }
+
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# Game Off 2020
+
+Submission to Game Off 2020 : https://itch.io/jam/game-off-2020
+
+Unity 2019.4.13 LTS with Universal Render Pipeline
+Git LFS required : https://git-lfs.github.com/
